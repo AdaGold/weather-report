0738f4bfa618f797ded87869371225c9be741e96
Lindsey Still <lindseymstill@gmail.com>
2022-06-11 01:04:17 +0900

    wave 4 complete

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
-
 const registerEventHandlers = () => {
-  document.getElementById("up-temp").addEventListener('click', increaseTemp);
-  document.getElementById("down-temp").addEventListener('click', decreaseTemp);
+  document.getElementById("down-temp").addEventListener('click', () => changeTemp(-1));
+  document.getElementById("up-temp").addEventListener('click', () => changeTemp(1));
   document.getElementById("city-search-input").addEventListener("search", changeCity)
-  document.getElementById("city-searh-button").addEventListener("click", toggleFunction)
+  document.getElementById("city-search-input").addEventListener("search", changeWeatherAsync)
+  document.getElementById("city-search-button").addEventListener("click", changeWeatherAsync)
+  document.getElementById("city-search-button").addEventListener("click", toggleFunction)
   //for Drop Down Menu Search
   // let links = document.getElementsByClassName("dropdown-item")
   // for (let i = 0; i<links.length; i++){
@@ -18,18 +19,16 @@ const registerEventHandlers = () => {
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
 
 const state = {
-  temperature: 60
+  temperature: 60,
+  cityName: 'Tokyo',
+  weatherDescription: "scattered clouds",
+  weatherIconName: "bi-cloud-lightning-rain",
+  oldIconName: "hello",
+  skyImgUrl: null  
 }
 
-const increaseTemp = () => {
-  state.temperature++
-  document.getElementById("temp").innerHTML = `${state.temperature}&deg;`
-  checkTextColorChange()
-  checkSeasonChange()
-};
-
-const decreaseTemp = () => {
-  state.temperature--
+const changeTemp = (change) => {
+  state.temperature += change
   document.getElementById("temp").innerHTML = `${state.temperature}&deg;`
   checkTextColorChange()
   checkSeasonChange()
@@ -40,10 +39,12 @@ const checkSeasonChange = () => {
     document.getElementById("temp-img").src = "/ada-project-docs/assets/winter_landscape.png";
   } else if (32 < state.temperature && state.temperature < 56){
     document.getElementById("temp-img").src = "/ada-project-docs/assets/fall_landscape.png";
-  } else if (56 <= state.temperature  && state.temperature < 80){
+  } else if (56 <= state.temperature  && state.temperature < 75){
     document.getElementById("temp-img").src = "/ada-project-docs/assets/spring_landscape.png";
-  } else {
+  } else if (75 <= state.temperature  && state.temperature < 95) {
     document.getElementById("temp-img").src = "/ada-project-docs/assets/summer_landscape.png";
+  }else {
+    document.getElementById("temp-img").src = "/ada-project-docs/assets/hottest_landscape.png";
   }
 };
 
@@ -62,11 +63,119 @@ const checkTextColorChange = () => {
 };
 
 const changeCity = () => {
-  document.getElementById("city-name").innerHTML = document.getElementById("city-search-input").value;
+  state.cityName = document.getElementById("city-search-input").value;
+  document.getElementById("city-name").textContent = state.cityName;
 };
 
 const toggleFunction = () => {
   document.getElementById("city-search-button").classList.toggle("show");
+}
+
+
+const weatherMainToIcon = {"Thunderstorm": ["bi-cloud-lightning-rain", "/ada-project-docs/assets/thunderstorm_sky.jpg"], "Drizzle": ["bi-cloud-drizzle", "/ada-project-docs/assets/rain_sky.jpg"], "Rain": ["bi-cloud-rain", "/ada-project-docs/assets/rain_sky.jpg"], "Snow": ["bi-cloud-snow", "/ada-project-docs/assets/snow_sky.jpg"], "Mist": ["bi-cloud-haze", "/ada-project-docs/assets/mist_sky.jpg"], "Smoke": ["bi-cloud-fog", "/ada-project-docs/assets/mist_sky.jpg"], "Haze": ["bi-cloud-haze", "/ada-project-docs/assets/mist_sky.jpg"], "Dust": ["bi-cloud-fog", "/ada-project-docs/assets/mist_sky.jpg"], "Fog": ["bi-cloud-haze", "/ada-project-docs/assets/mist_sky.jpg"], "Sand": ["bi-cloud-fog", "/ada-project-docs/assets/mist_sky.jpg"], "Dust": ["bi-cloud-fog", "/ada-project-docs/assets/mist_sky.jpg"], "Ash": ["bi-cloud-fog", "/ada-project-docs/assets/mist_sky.jpg"], "Squall": ["bi-cloud-fog", "/ada-project-docs/assets/mist_sky.jpg"], "Tornado": ["bi-cloud-fog", "/ada-project-docs/assets/mist_sky.jpg"], "Clear": ["bi-sun", "/ada-project-docs/assets/clear_sky.jpg"], "Clouds": ['bi-clouds', "/ada-project-docs/assets/broken_clouds_sky.jpg"]}
+
+const changeWeatherAsync = async () => {
+  const q = state.cityName;
+  try{
+    let response = await axios.get("http://localhost:5000/location", {
+      params: {
+        q
+      }
+    });
+    const lat = response.data[0].lat;
+    const lon = response.data[0].lon;
+    console.log('success!', response.data);
+    try{
+      let weatherResponse = await axios.get("http://localhost:5000/weather", {
+        params: {
+          lat,
+          lon
+        }
+      });
+        updateState(weatherResponse);
+        updateUI();
+      }catch(error){
+        console.log('weather error!', error)
+      }
+  }catch(error){
+    console.log('location error', error)
+  };
+};
+// const changeWeather = () => {
+//   const q = state.cityName;
+
+  // axios.get("http://localhost:5000/location", {
+  //   params: {
+  //     q
+  //   }
+  // })
+  // .then((response) => {
+  //   const lat = response.data[0].lat;
+  //   const lon = response.data[0].lon;
+  //   console.log('success!', response.data);
+  //   axios.get("http://localhost:5000/weather", {
+  //     params: {
+  //       lat,
+  //       lon
+  //     }
+  //   })
+    // .then((response) => {
+    //   //Store response data
+    //   state.temperature = Math.round(response.data.current.temp)
+    //   state.weatherDescription = response.data.current.weather[0].description;
+    //   state.oldIconName = state.weatherIconName;
+    //   state.weatherIconName = weatherMainToIcon[response.data.current.weather[0].main][0];
+    //   state.skyImgUrl = weatherMainToIcon[response.data.current.weather[0].main][1];
+    //   console.log('successfully stored response data!', response.data);
+    // })
+    // .then(() => {
+    //   //Update UI
+    //   document.getElementById("temp").innerHTML = `${state.temperature}&deg;`;
+    //   checkTextColorChange();
+    //   checkSeasonChange();
+    //   setWeatherIcon()
+    //   setSky();
+    //   setWeatherDescription();
+    //   console.log('successfully updated UI!');
+    // })
+//     .catch((error) => {
+//       console.log('error!', error)
+//     });
+//   })
+//   .catch((error) => {
+//     console.log('error!', error)
+//   });
+// }
+
+const updateState = (weatherResponse) => {
+  state.temperature = Math.round(weatherResponse.data.current.temp);
+  state.weatherDescription = weatherResponse.data.current.weather[0].description;
+  state.oldIconName = state.weatherIconName;
+  state.weatherIconName = weatherMainToIcon[weatherResponse.data.current.weather[0].main][0];
+  state.skyImgUrl = weatherMainToIcon[weatherResponse.data.current.weather[0].main][1];
+  console.log('successfully stored response data!', weatherResponse.data);
+}
+
+const updateUI = () => {
+  document.getElementById("temp").innerHTML = `${state.temperature}&deg;`;
+  checkTextColorChange();
+  checkSeasonChange();
+  setWeatherIcon();
+  setSky();
+  setWeatherDescription();
+  console.log('successfully updated UI!');
+}
+
+const setSky = () => {
+  document.body.style.background = `url(${state.skyImgUrl}) no-repeat top fixed`;
+  document.body.style.backgroundSize = '100% 100%';
+}
+const setWeatherIcon = () => {
+  document.getElementsByClassName("weather-icon")[1].classList.replace(state.oldIconName, state.weatherIconName);
+}
+
+const setWeatherDescription = () => {
+  document.getElementById("wdescription").innerHTML = state.weatherDescription;
 }
 
 //For Drop Down Menu Search
