18958fa2366f8409bfbd480783baefd2b813d1f7
Ariel <gabriellaw15@gmail.com>
2022-12-07 15:31:01 -0800

    Creatd helper function to change temperature color based on temp

--- src/index.js
+++ src/index.js
@@ -3,25 +3,33 @@
 const increaseTemp = () => {
   const currentTemp = document.getElementById('temp');
   currentTemp.textContent = parseInt(currentTemp.textContent) + 1;
+  colorTempChange(currentTemp);
 };
 
 const decreaseTemp = () => {
   const currentTemp = document.getElementById('temp');
   currentTemp.textContent = parseInt(currentTemp.textContent) - 1;
+  colorTempChange(currentTemp);
 };
 
-const changeColor = () => {
-  const currentTemp = document.getElementById('temp');
-  if (parseInt(currentTemp.textContent) >= 80) {
-    currentTemp.style.color = '#FF0000';
+const colorTempChange = (temp) => {
+  if (parseInt(temp.textContent) >= 80) {
+    temp.style.color = '#FF0000';
+  } else if (parseInt(temp.textContent) >= 70) {
+    temp.style.color = '#FFA500';
+  } else if (parseInt(temp.textContent) >= 60) {
+    temp.style.color = '#D1D100';
+  } else if (parseInt(temp.textContent) >= 50) {
+    temp.style.color = '#00FF00';
+  } else if (parseInt(temp.textContent) <= 49) {
+    temp.style.color = '#00AEAE';
   }
 };
 
 const registerEventHandlers = () => {
   const increaseButton = document.getElementById('increase-temp');
   const decreaseButton = document.getElementById('decrease-temp');
-  const onColorChange = document.getElementById('temp');
-  onColorChange.addEventListener('onchange', changeColor);
+
   increaseButton.addEventListener('click', increaseTemp);
   decreaseButton.addEventListener('click', decreaseTemp);
 };
