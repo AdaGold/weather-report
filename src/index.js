f73d53b5bb9f66cc118976771bf229ff12792b74
Jamie <jamie.danielle@denverrollerderby.org>
2023-06-08 10:22:20 -0600

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
-    console.log("myFunctionCalled");
-  };
-  function currentTemp() {
-    document.getElementById("demo").innerHTML = "12";
-    console.log("myFunctionCalled");
-  };
-
 const getCurrentTime = () => {
     const currentDate = new Date();
     console.log("getCurrentTime");
     return currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
 }
 console.log(`The current time is ${getCurrentTime()}.`);
-
-// const increaseTempControl = () => {
-//     const increaseTemp = document.createElement("span");
-//     const tempContainer = document.querySelector("#tempContainer");
-
-// }
 
 const registerEventHandlers = () => {
     console.log("registerEventHandlers");
