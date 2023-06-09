032ecd35fca64a33fb7745d2568b75f60147f0a6
Allie Soliz <solizale@gmail.com>
2023-06-08 20:44:39 -0400

    adds functionality to increase temperature with up-arrow button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Selects the HTML Elements the events will occur on
+const increaseTempButton = document.querySelector('#increaseTempControl');
+
+// Makes functions to run when events occur
+const state = {
+	temp: 0,
+};
+
+// Sets the temperate value to state
+const updateTemp = () => {
+	const tempValue = document.querySelector('#tempValue');
+	tempValue.textContent = `${state.temp}`;
+};
+
+const increaseTemp = () => {
+	state.temp += 1;
+	updateTemp();
+};
+
+// Registers functions as 'event listeners'
+increaseTempButton.addEventListener('click', increaseTemp);
